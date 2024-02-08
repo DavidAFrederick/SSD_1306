9e08b01ff5ad5430b1f5b7ae6e0fead09c5397bd
David Frederick <DavidAFrederick.va.usa.earth@gmail.com>
2024-02-08 13:36:31 -0500

    Update SSD_1306_A.py

--- SSD_1306_A.py
+++ SSD_1306_A.py
@@ -13,9 +13,7 @@ BORDER = 5
 
 # Use for I2C.
 i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)  # Removed reset
-# oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
 
 
 # Clear display.
@@ -29,36 +27,36 @@ image = Image.new("1", (oled.width, oled.height))
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
-# Draw a white background
-draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+# # Draw a white background
+# draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
 
-
-
-# Draw a smaller inner rectangle
-draw.rectangle(
-    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-    outline=0,
-    fill=0,
-)
+# # Draw a smaller inner rectangle
+# draw.rectangle(
+#     (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+#     outline=0,
+#     fill=0,
+# )
 
 # Load default font.
 font = ImageFont.load_default()
 
 # Draw Some Text
+# Documentation:  https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html
+
 text = "CMD 10"
+draw.text((0,0), text, font=font,fill=255,)
+
 #(font_width, font_height) = font.getsize(text)
-font_width=5
-font_height = 8
-draw.text(
-    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
-    text,
-    font=font,
-    fill=255,
-)
+# font_width=5
+# font_height = 8
+# draw.text(
+#     (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+#     text,
+#     font=font,
+#     fill=255,
+# )
 
 # Display image
 oled.image(image)
 oled.show()
 
-# oled.fill(0)
-# oled.show()
